bb7150a3f15e53ccf69dc9fa1851b05c3c0dc83c
valens <ukwitegetsev9@gamail.com>
2025-04-03 14:21:21 +0200

    validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:health_assistant_app/HomePages/login.dart';
-import 'package:health_assistant_app/HomePages/AdminDashboard.dart';
-import 'package:health_assistant_app/HomePages/ParentDashboard.dart';
-import 'package:health_assistant_app/HomePages/UmunyabuzimaDashboard.dart';
-import 'package:health_assistant_app/HomePages/forgetpassword.dart';
 import 'package:health_assistant_app/HomePages/signup.dart';
+import 'package:health_assistant_app/healthcare/healthCareDashboard.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'WellnessBridge',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => LoginPage(),
-        '/adminDashboard': (context) => AdminDashboard(),
-        '/parentDashboard': (context) => ParentDashboard(),
-        '/umunyabuzimaDashboard': (context) => UmunyabuzimaDashboard(),
-        '/forgetPassword': (context) => ForgetPasswordPage(),
-        '/signUp': (context) => SignUpPage(),
-      },
-    );
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/login',
+    routes: {
+      '/login': (context) => LoginPage(),
+      '/signup': (context) => SignUpPage(),
+      '/dashboard': (context) => HealthCareDashboard(),
+    },
+  ));
 }
